081851cafd0fdeac1f89acb7e1f1dc4ae675cf72
IsaForsberg <isa.forsberg@gritacademy.se>
2025-11-25 18:39:38 +0100

    move addToCart to cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,7 @@
 
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
+
 
 let productsHTML = '';
 
@@ -55,41 +56,28 @@ products.forEach((products)=>{
             Add to Cart
           </button>
         </div>`;
-
-       
 });
 
  console.log(productsHTML);
 
  document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
- document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
-  button.addEventListener('click', ()=>{
-   const productId = button.dataset.productId;
-
-   let matchingItem;
-
-    cart.forEach((item)=>{
-      if(productId === item.productId){
-        matchingItem = item;
-      }
-    });
-
-    if (matchingItem){
-      matchingItem.quantity += 1;
-    } else {cart.push ({
-    productId: productId,
-    quantity: 1
-   });
-  }
-
+ function updateCartQuantity (){
   let cartQuantity = 0;
 
-  cart.forEach((item)=>{
-    cartQuantity += item.quantity;
+  cart.forEach((cartItem)=>{
+    cartQuantity += cartItem.quantity;
   });
 
   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+ }
+
+ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+   button.addEventListener('click', ()=>{
+       const productId = button.dataset.productId;
+    addToCart(productId);
+
+   updateCartQuantity();
   });
  });
 

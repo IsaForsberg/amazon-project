973c88d192504f633ec547ea94f80cecd3fb7d18
IsaForsberg <isa.forsberg@gritacademy.se>
2025-11-30 19:55:29 +0100

    use inheritance and polymorphism to include sice chart

--- data/products.js
+++ data/products.js
@@ -1,7 +1,5 @@
 import {formatCurrency} from "../scripts/utils/money.js";
 
-
-
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -36,7 +34,28 @@ return `images/ratings/rating-${this.rating.stars * 10}.png`
 getPrice(){
  return `$${formatCurrency(this.priceCents)}`
 }
+
+extraInfoHTML(){
+return '';
 }
+}
+class Clothing extends Product {
+ sizeChartLink;
+ 
+ constructor(productDetails){
+  super(productDetails);
+  this.sizeChartLink = productDetails.sizeChartLink;
+ }
+
+ extraInfoHTML(){
+  return `
+  <a href="${this.sizeChartLink}" target="_blank">
+        Size chart
+      </a>
+  `;
+ }
+}
+
 
 export const products = [
   {
@@ -698,6 +717,9 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+  if (productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
 return new Product(productDetails);
 });
 

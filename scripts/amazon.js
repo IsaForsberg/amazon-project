94ee4d6b0bc8ae288ad0369f3dd24c2e2054481d
IsaForsberg <isa.forsberg@gritacademy.se>
2025-12-01 16:38:47 +0100

    load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,88 +1,94 @@
 import {cart, addToCart} from '../data/cart.js';
-import {products} from '../data/products.js';
+import {products, loadProducts} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
-let productsHTML = '';
+loadProducts(renderProductsGrid);
 
-products.forEach((product) => {
-  productsHTML += `
-    <div class="product-container">
-      <div class="product-image-container">
-        <img class="product-image"
-          src="${product.image}">
-      </div>
+function renderProductsGrid(){
 
-      <div class="product-name limit-text-to-2-lines">
-        ${product.name}
-      </div>
 
-      <div class="product-rating-container">
-        <img class="product-rating-stars"
-          src="${product.getStarsUrl()}">
-        <div class="product-rating-count link-primary">
-          ${product.rating.count}
-        </div>
-      </div>
 
-      <div class="product-price">
-       ${product.getPrice()}
-      </div>
+    let productsHTML = '';
 
-      <div class="product-quantity-container">
-  <select class="js-quantity-selector" data-product-id="${product.id}">
-    ${[1,2,3,4,5,6,7,8,9,10].map(num => `
-      <option value="${num}" ${num === 1 ? 'selected' : ''}>${num}</option>
-    `).join('')}
-  </select>
-</div>
+    products.forEach((product) => {
+      productsHTML += `
+        <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
 
-        ${product.extraInfoHTML()}
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
 
-      <div class="product-spacer"></div>
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="${product.getStarsUrl()}">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
 
-      <div class="added-to-cart">
-        <img src="images/icons/checkmark.png">
-        Added
-      </div>
+          <div class="product-price">
+          ${product.getPrice()}
+          </div>
 
-      <button class="add-to-cart-button button-primary js-add-to-cart"
-      data-product-id="${product.id}">
-        Add to Cart
-      </button>
+          <div class="product-quantity-container">
+      <select class="js-quantity-selector" data-product-id="${product.id}">
+        ${[1,2,3,4,5,6,7,8,9,10].map(num => `
+          <option value="${num}" ${num === 1 ? 'selected' : ''}>${num}</option>
+        `).join('')}
+      </select>
     </div>
-  `;
-});
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+            ${product.extraInfoHTML()}
 
-function updateCartQuantity() {
-  let cartQuantity = 0;
+          <div class="product-spacer"></div>
 
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
 
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
-}
-
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
-
-      // hitta rätt select för denna produkt
-      const select = document.querySelector(`.js-quantity-selector[data-product-id="${productId}"]`);
-      const quantity = Number(select.value);
-
-      // lägg till i cart med rätt quantity
-      for (let i = 0; i < quantity; i++) {
-        addToCart(productId);
-      }
-
-      updateCartQuantity();
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id="${product.id}">
+            Add to Cart
+          </button>
+        </div>
+      `;
     });
-  });
 
+    document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+    function updateCartQuantity() {
+      let cartQuantity = 0;
+
+      cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+    }
+
+    document.querySelectorAll('.js-add-to-cart')
+      .forEach((button) => {
+        button.addEventListener('click', () => {
+          const productId = button.dataset.productId;
+
+          // hitta rätt select för denna produkt
+          const select = document.querySelector(`.js-quantity-selector[data-product-id="${productId}"]`);
+          const quantity = Number(select.value);
+
+          // lägg till i cart med rätt quantity
+          for (let i = 0; i < quantity; i++) {
+            addToCart(productId);
+          }
+
+          updateCartQuantity();
+        });
+      });
+
+}
 
